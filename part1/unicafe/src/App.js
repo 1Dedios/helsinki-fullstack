d28a0e43f7e258e2aea53b44a3763f4a8aced5f0
Dedios <dediosprogramming@gmail.com>
2023-06-02 21:08:00 -0400

    feedback buttons created

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,6 +1,14 @@
+const Heading = () => <h1>give feedback</h1>;
+
+const Button = ({ text }) => <button>{text}</button>;
+
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Heading />
+      <Button text={"good"} />
+      <Button text={"neutral"} />
+      <Button text={"bad"} />
       <h1>hey</h1>
     </div>
   );
