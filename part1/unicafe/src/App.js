3d08f65a1dcbaed3a85bec101594d69dd47c60d1
Dedios <dediosprogramming@gmail.com>
2023-06-02 22:20:50 -0400

    eventhandler added

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,15 +1,39 @@
-const Heading = () => <h1>give feedback</h1>;
+import { useState } from "react";
 
-const Button = ({ text }) => <button>{text}</button>;
+const Heading = ({ text }) => <h1>{text}</h1>;
 
-function App() {
+const Button = ({ text, handleClick }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const Statistics = () => {
+  // hold state here and render in return
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
   return (
     <div>
-      <Heading />
-      <Button text={"good"} />
+      <p>good</p>
+      <p>neutral</p>
+      <p>bad</p>
+    </div>
+  );
+};
+
+function App() {
+  // handleclick functions are here. remember eventhandlers are functions
+
+  const handleClick = () => console.log("you pressed me");
+
+  return (
+    <div>
+      <Heading text={"give feedback"} />
+      <Button handleClick={handleClick} text={"good"} />
       <Button text={"neutral"} />
       <Button text={"bad"} />
-      <h1>hey</h1>
+      <Heading text={"statistics"} />
+      <Statistics />
     </div>
   );
 }
