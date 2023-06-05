a34dc5982b916c1995f89ad318981558682868e1
Dedios <dediosprogramming@gmail.com>
2023-06-04 21:43:06 -0400

    implemented state in the app

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,34 +6,29 @@ const Button = ({ text, handleClick }) => {
   return <button onClick={handleClick}>{text}</button>;
 };
 
-const Statistics = () => {
-  // hold state here and render in return
+const Statistics = ({ good, neutral, bad }) => {
+  return (
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  );
+};
+
+function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
   return (
     <div>
-      <p>good</p>
-      <p>neutral</p>
-      <p>bad</p>
-    </div>
-  );
-};
-
-function App() {
-  // handleclick functions are here. remember eventhandlers are functions
-
-  const handleClick = () => console.log("you pressed me");
-
-  return (
-    <div>
       <Heading text={"give feedback"} />
-      <Button handleClick={handleClick} text={"good"} />
-      <Button text={"neutral"} />
-      <Button text={"bad"} />
+      <Button handleClick={() => setGood(good + 1)} text={"good"} />
+      <Button handleClick={() => setNeutral(neutral + 1)} text={"neutral"} />
+      <Button handleClick={() => setBad(bad + 1)} text={"bad"} />
       <Heading text={"statistics"} />
-      <Statistics />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 }
