8255f4dfd878db11862eca328098b5bd64ddaed5
Dedios <dediosprogramming@gmail.com>
2023-06-07 22:09:35 -0400

    implemented all state

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,7 +6,7 @@ const Button = ({ text, handleClick }) => {
   return <button onClick={handleClick}>{text}</button>;
 };
 
-const Statistics = ({ good, neutral, bad, all, average, positive }) => {
+const Statistics = ({ good, neutral, bad, allClicks, average, positive }) => {
   return (
     <div>
       <p>
@@ -15,7 +15,7 @@ const Statistics = ({ good, neutral, bad, all, average, positive }) => {
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
       <p>
-        all <span id="all">{all}</span>
+        all <span>{allClicks.length}</span>
       </p>
       <p>average {average}</p>
       <p>positive {positive}</p>
@@ -27,34 +27,45 @@ function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [all, setAll] = useState(0);
+  const [allClicks, setAllClicks] = useState([]);
   const [average, setAverage] = useState(0);
   const [positive, setPositive] = useState(0);
-
-  {
-    /* I think the way to go with all, positive and average is adding an event listener. I just don't know how to access it. I can access the element in Statistics component but the setting function for state exists in the App component 🤔 */
-  }
-
-  const allCount = document.getElementById("all");
-
-  allCount.addEventListener("click", setAll(all + 1));
-
-  // setAll(() => {
-  //   all + 1;
-  // });
 
   return (
     <div>
       <Heading text={"give feedback"} />
-      <Button handleClick={() => setGood(good + 1)} text={"good"} />
-      <Button handleClick={() => setNeutral(neutral + 1)} text={"neutral"} />
-      <Button handleClick={() => setBad(bad + 1)} text={"bad"} />
+
+      <Button
+        handleClick={() => {
+          setAllClicks(allClicks.concat("c"));
+          setGood(good + 1);
+        }}
+        text={"good"}
+      />
+
+      <Button
+        handleClick={() => {
+          setAllClicks(allClicks.concat("c"));
+          setNeutral(neutral + 1);
+        }}
+        text={"neutral"}
+      />
+
+      <Button
+        handleClick={() => {
+          setAllClicks(allClicks.concat("c"));
+          setBad(bad + 1);
+        }}
+        text={"bad"}
+      />
+
       <Heading text={"statistics"} />
+
       <Statistics
         good={good}
         neutral={neutral}
         bad={bad}
-        all={all}
+        allClicks={allClicks}
         average={average}
         positive={positive}
       />
